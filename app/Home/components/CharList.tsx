25881f3c7e2b158a5423dfd174d3265ce41f3792
gabyakd2 <gabrielsal00@hotmail.com>
2023-11-06 17:07:24 -0300

    fix and add: refactoring code and create Context

--- app/Home/components/CharList.tsx
+++ app/Home/components/CharList.tsx
@@ -1,28 +1,11 @@
 "use client"
-import { useEffect, useState } from "react";
 import CardChar from "./CardChar";
 import { ListCardContainer, ParentContainer } from "@/styledComponents/StyledComponents";
 import { IDataCharacter } from "@/interfaces/ICharacter";
+import useFetchCharacters from "@/app/hooks/useFetchChar";
 
 function CharList() {
-  const [dataCharacter, setDataCharacter] = useState<IDataCharacter[] | null>(null);
-  useEffect(() => {
-    const fetchCharacters = async () => {
-      try {
-        const response = await fetch(
-          "https://rickandmortyapi.com/api/character"
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const responseData = await response.json();
-        setDataCharacter(responseData.results);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchCharacters();
-  }, []);
+  const { dataCharacter } = useFetchCharacters();
   return (
     <ParentContainer>
     <ListCardContainer>
